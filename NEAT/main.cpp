6f99154f5cf5ee5bf8a83ffc9f3c3592038c047d
SacredArkonite <miclepic1@gmail.com>
2018-06-11 23:10:55 -0400

    Completed the mutation using a multimap

--- NEAT/main.cpp
+++ NEAT/main.cpp
@@ -2,20 +2,27 @@
 #include<vector>
 #include<memory>
 #include<algorithm>
+#include<random>
+#include<map>
+
+typedef uint8_t N_SIZE;
+typedef uint16_t N_SIZEx2;
+typedef uint32_t C_SIZE;
 
 struct Genome
 {
-	std::vector<int> sourceGene;
-	std::vector<int> destGene;
-	std::vector<int> history;
-	std::vector<int> disabledIndex;
+	std::vector<N_SIZE> sourceNode;
+	std::vector<N_SIZE> destNode;
+	std::vector<C_SIZE> history;
+	std::vector<C_SIZE> disabledIndex;
 	std::vector<float> weights;
-	int nodes = 0;
+	N_SIZE nodes = 0;
 	size_t evolutionHash = 0;
 };
-typedef std::unique_ptr<std::vector<std::unique_ptr<Genome>>> POP_PTR;
+typedef std::unique_ptr<Genome> GEN_PTR;
+typedef std::unique_ptr<std::vector<GEN_PTR>> POP_PTR;
 
-size_t hashGenetics(const std::vector<int>& history) {
+size_t hashGenetics(const std::vector<C_SIZE>& history) {
 	std::hash<int> hasher;
 	size_t newhash = 0;
 	for (auto it = history.begin(); it != history.end(); it++) {
@@ -24,7 +31,7 @@ size_t hashGenetics(const std::vector<int>& history) {
 	return newhash;
 }
 
-size_t hashGenetics(const size_t oldHash, const std::vector<int>& newHist) {
+size_t hashGenetics(const size_t oldHash, const std::vector<C_SIZE>& newHist) {
 	std::hash<int> hasher;
 	size_t newHash = oldHash;
 	for (auto it = newHist.begin(); it != newHist.end(); it++)
@@ -34,7 +41,7 @@ size_t hashGenetics(const size_t oldHash, const std::vector<int>& newHist) {
 	return newHash;
 }
 
-size_t hashGenetics(const size_t oldHash, const int newHist1, const int newHist2) {
+size_t hashGenetics(const size_t oldHash, const C_SIZE newHist1, const C_SIZE newHist2) {
 	std::hash<int> hasher;
 	size_t newHash = oldHash;
 	newHash ^= hasher(newHist1) + 0x9e3779b9 + (newHash << 6) + (newHash >> 2);
@@ -42,15 +49,15 @@ size_t hashGenetics(const size_t oldHash, const int newHist1, const int newHist2
 	return newHash;
 }
 
-std::unique_ptr<Genome> CreateGenome(const int nIns, const int nOuts)
+GEN_PTR CreateGenome(const N_SIZE nIns, const N_SIZE nOuts)
 {
 	auto genome = std::make_unique<Genome>();
-	int hist = 1;
+	C_SIZE hist = 1;
 	genome->nodes = nIns + nOuts;
-	for (int i = 1; i <= nIns; i++) {
-		for (int j = nIns + 1; j <= genome->nodes; j++) {
-			genome->sourceGene.push_back(i);
-			genome->destGene.push_back(j);
+	for (N_SIZE i = 1; i <= nIns; i++) {
+		for (N_SIZE j = nIns + 1; j <= genome->nodes; j++) {
+			genome->sourceNode.push_back(i);
+			genome->destNode.push_back(j);
 			genome->weights.push_back(0.0);
 			genome->history.push_back(hist++);
 		}
@@ -60,53 +67,47 @@ std::unique_ptr<Genome> CreateGenome(const int nIns, const int nOuts)
 	return genome;
 }
 
-std::unique_ptr<Genome> MutateAddConnection(std::unique_ptr<Genome> genome, const int from, const int to, const int histNb)
+GEN_PTR MutateAddConnection(GEN_PTR genome, const N_SIZE from, const N_SIZE to, const C_SIZE histNb)
 {
 	genome->history.push_back(histNb);
-	genome->sourceGene.push_back(from);
-	genome->destGene.push_back(to);
+	genome->sourceNode.push_back(from);
+	genome->destNode.push_back(to);
 	genome->weights.push_back(0.0);
 
 	return genome;
 }
 
-std::unique_ptr<Genome> MutateAddNode(std::unique_ptr<Genome> genome, const int index, const int histNb1, const int histNb2)
+GEN_PTR MutateAddNode(GEN_PTR genome, const C_SIZE index, const C_SIZE histNb)
 {
 	genome->disabledIndex.push_back(index);
 	genome->nodes++;
-	genome->history.push_back(histNb1);
-	genome->history.push_back(histNb2);
+	genome->history.push_back(histNb);
+	genome->history.push_back(histNb+1);
 
-	genome->sourceGene.push_back(genome->sourceGene[index]);
-	genome->destGene.push_back(genome->nodes);
+	genome->sourceNode.push_back(genome->sourceNode[index]);
+	genome->destNode.push_back(genome->nodes);
 	genome->weights.push_back(1.0);
 
-	genome->sourceGene.push_back(genome->nodes);
-	genome->destGene.push_back(genome->destGene[index]);
+	genome->sourceNode.push_back(genome->nodes);
+	genome->destNode.push_back(genome->destNode[index]);
 	genome->weights.push_back(genome->weights[index]);
 
-	genome->evolutionHash = hashGenetics(genome->evolutionHash, histNb1, histNb2);
+	genome->evolutionHash = hashGenetics(genome->evolutionHash, histNb, histNb+1);
 
 	return genome;
 }
 
-POP_PTR CreatePop(const int popSize, const int inputs, const int outputs)
+POP_PTR CreatePop(const int popSize, const N_SIZE inputs, const N_SIZE outputs, C_SIZE& hist)
 {
-	POP_PTR pop = std::make_unique<std::vector<std::unique_ptr<Genome>>>();
+	POP_PTR pop = std::make_unique<std::vector<GEN_PTR>>();
 
 	for (int i = 0; i < popSize; i++)
 	{
 		pop->push_back(CreateGenome(inputs, outputs));
 	}
 
+	hist = (*pop)[0]->history.size() + 1;
 	return pop;
-}
-
-void MutatePop(std::vector<std::unique_ptr<Genome>>& pop)
-{
-	pop[2] = MutateAddNode(std::move(pop[2]), 2, 4, 5);
-	pop[3] = MutateAddNode(std::move(pop[3]), 2, 4, 5);
-	pop[4] = MutateAddNode(std::move(pop[4]), 0, 6, 7);
 }
 
 float Compatibility(const float c1, const float c2, const float c3, const Genome& genome1, const Genome& genome2)
@@ -156,12 +157,12 @@ float Compatibility(const float c1, const float c2, const float c3, const Genome
 	return (c1*excess + c2*disjoint + c3*d_sum/matching);
 }
 
-POP_PTR GenerateExample()
+POP_PTR GenerateExample(C_SIZE& hist)
 {
-	POP_PTR pop = CreatePop(2, 3, 1);
-	(*pop)[0] = MutateAddNode(std::move((*pop)[0]), 1, 4, 5);
-	(*pop)[1] = MutateAddNode(std::move((*pop)[1]), 1, 4, 5);
-	(*pop)[1] = MutateAddNode(std::move((*pop)[1]), 4, 6, 7);
+	POP_PTR pop = CreatePop(2, 3, 1, hist);
+	(*pop)[0] = MutateAddNode(std::move((*pop)[0]), 1, 4);
+	(*pop)[1] = MutateAddNode(std::move((*pop)[1]), 1, 4);
+	(*pop)[1] = MutateAddNode(std::move((*pop)[1]), 4, 6);
 	(*pop)[0] = MutateAddConnection(std::move((*pop)[0]), 1, 5, 8);
 	(*pop)[1] = MutateAddConnection(std::move((*pop)[1]), 3, 5, 9);
 	(*pop)[1] = MutateAddConnection(std::move((*pop)[1]), 1, 6, 10);
@@ -169,16 +170,139 @@ POP_PTR GenerateExample()
 	return pop;
 }
 
+bool CheckIfConnectionExists(const std::vector<GEN_PTR>::iterator it, const std::pair<N_SIZE, N_SIZE> connection)
+{
+	auto sr_begin = (*it)->sourceNode.begin();
+	auto sr_ptr = sr_begin;
+	auto sr_end = (*it)->sourceNode.end();
+	C_SIZE index = -1; // We crash if not found
+	for (; sr_ptr != sr_end; sr_ptr++) {
+		if ((*sr_ptr) == connection.first)
+		{
+			index = sr_ptr - (*it)->sourceNode.begin();
+			if ((*it)->destNode[index] == connection.second)
+			{	//FOUND IT!
+				//Remove from the disabled list if its there
+				auto pos_in_disabled = std::find((*it)->disabledIndex.begin(), (*it)->disabledIndex.end(), index);
+				if (pos_in_disabled != (*it)->disabledIndex.end())
+					(*it)->disabledIndex.erase(pos_in_disabled);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+POP_PTR MutatePop(POP_PTR pop, float new_node_percent, float new_link_percent, C_SIZE& hist)
+{
+	//making a new node and a new connection should be mutually exclusive
+	new_link_percent = new_link_percent + new_node_percent;
+
+
+	//Prepping next gen
+	POP_PTR nexxgen = std::make_unique<std::vector<GEN_PTR>>();
+
+	//We use a multimap to order the genome by their {type,operation} 
+	//in order to identify common types after the transform
+	//so we can give the same history number to the same transform
+	std::multimap<std::pair<size_t, C_SIZE>, GEN_PTR> mm_node;
+	std::multimap<std::pair<size_t, std::pair<N_SIZE, N_SIZE>>, GEN_PTR> mm_conn;
+
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<unsigned int> rngDist(0, -1);
+	auto end = (*pop).end();
+	for (auto it = (*pop).begin(); it < end; it++)
+	{
+		float rn = (rngDist(rng) % 256) / 256.0f;
+		if (rn < new_node_percent)
+		{	// Mutate Add Node
+			//*it = MutateAddNode(std::move(*it), 1, 1, 1);
+
+			size_t hash = (*it)->evolutionHash;
+			C_SIZE connection_id = rngDist(rng) % ((*it)->history.size());
+			auto key = std::pair<size_t, C_SIZE>(hash, connection_id);
+			mm_node.insert({ key, std::move(*it) });
+		}
+		else if (rn < new_link_percent)
+		{	// Mutate Add Connection
+			//*it = MutateAddConnection(std::move(*it), 1, 1, 2);
+
+			size_t hash = (*it)->evolutionHash;
+			N_SIZE nb_nodes = (*it)->nodes;
+			auto connection = std::pair<N_SIZE, N_SIZE>(rngDist(rng) % nb_nodes, rngDist(rng) % nb_nodes);
+			
+			//Check if this connection already exists
+			if (CheckIfConnectionExists(it, connection))
+			{
+				nexxgen->push_back(std::move(*it));
+			}
+			else
+			{
+				auto key = std::pair<size_t, std::pair<N_SIZE, N_SIZE>>(hash, connection);
+				mm_conn.insert({ key, std::move(*it) });
+			}
+		}
+		else
+		{
+			// Keep as is
+			nexxgen->push_back(std::move(*it));
+		}
+
+	}
+
+	//iterate over the new nodes multimap in key order
+	if (!mm_node.empty())
+	{
+		std::multimap<std::pair<size_t, C_SIZE>, GEN_PTR>::iterator it = mm_node.begin();
+		std::multimap<std::pair<size_t, C_SIZE>, GEN_PTR>::iterator end = mm_node.end();
+		std::pair<size_t, C_SIZE> last_key;
+		for (last_key = it->first ; it != end; it++) {
+			//Increment the mutation ID if when needed
+			if (it->first != last_key)
+			{
+				last_key = it->first;
+				hist += 2;
+			}
+			nexxgen->push_back(MutateAddNode(std::move(it->second),last_key.second,hist));
+		}
+
+		hist += 2;
+	}
+
+	//iterate over the new conx multimap in key order
+	if (!mm_conn.empty())
+	{
+		std::multimap<std::pair<size_t, std::pair<N_SIZE,N_SIZE>>, GEN_PTR>::iterator it = mm_conn.begin();
+		std::multimap<std::pair<size_t, std::pair<N_SIZE, N_SIZE>>, GEN_PTR>::iterator end = mm_conn.end();
+		std::pair<size_t, std::pair<N_SIZE, N_SIZE>> last_key = it->first;
+		for (; it != end; it++) {
+			//Increment the mutation ID if when needed
+			if (it->first != last_key)
+			{
+				last_key = it->first;
+				hist ++;
+			}
+			nexxgen->push_back(MutateAddConnection(std::move(it->second), last_key.second.first, last_key.second.second, hist));
+		}
+
+		hist++;
+	}
+
+	return nexxgen;
+}
+
 int main()
 {
 	std::cout << "hello bitches!" << std::endl;
 
-	POP_PTR pop = GenerateExample();
+	//POP_PTR pop = GenerateExample();
 
-	float delta = Compatibility(1, 1, 1, *(*pop)[0], *(*pop)[1]);
-	
-	//POP_PTR pop = CreatePop(100, 3, 1);
-	//MutatePop(*pop);
+	//float delta = Compatibility(1, 1, 1, *(*pop)[0], *(*pop)[1]);
+
+	C_SIZE hist;
+	POP_PTR pop = CreatePop(10, 2, 1, hist);
+	pop = MutatePop(std::move(pop),0.25,0.25,hist);
 	//size_t type0 = pop->at(0)->evolutionHash;
 	//size_t type1 = pop->at(1)->evolutionHash;
 	//size_t type2 = pop->at(2)->evolutionHash;
